a5572725950056659a622ed588e086db289840fe
Jorge Duarte <sadujorge@gmail.com>
2024-03-12 21:58:28 -0700

    Added typeDefs for games, users, and reviews as well as for a couple of queries to retrieve user, game, and review data using graphQl layer

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,52 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    email: String!
+    username: String!
+    password: String!
+    profilePictureUrl: String
+    gamesInFavorites: [Game]
+    gamesInBacklog: [Game]
+    gamesCompleted: [Game]
+    gamesInProgress: [Game]
+    friends: [User]
+    likedReviews: [Review]
+    reviews: [Review]
+    hoursPlayed: Int
+    games100Completed: [Game]
+  }
+
+  type Review {
+    id: ID!
+    user: User!
+    game: Game!
+    rating: Float!
+    likes: Int
+    dateOfReview: String
+    reviewText: String!
+  }
+
+  type Game {
+    id: ID!
+    title: String!
+    averageRating: Float
+    releaseDate: String
+    genre: [String]
+    platforms: [String]
+    pictureUrl: String
+    reviews: [Review]
+  }
+
+  type Query {
+    users: [User]
+    user(id: ID!): User
+    games: [Game]
+    game(id: ID!): Game
+    reviews: [Review]
+    review(id: ID!): Review
+  }
+`;
+
+module.exports = typeDefs;
