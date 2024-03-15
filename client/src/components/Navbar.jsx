b02fc95f78789dcea422ca770f5c4cbf3a6bd5ce
Jorge Duarte <sadujorge@gmail.com>
2024-03-14 22:15:28 -0700

    Added responsiveness to navbar and path for SearchPage.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -29,9 +29,8 @@ function App() {
         <nav className="navbar-navbar">
           <div className="navbar-container-menu-items">
             <Link to="/home" style={{ color: location.pathname === '/home' ? '#9F34FF' : '#ffffff','--item': '0' }}>Home</Link>
-          </div>
-          <div className="navbar-container-menu-items">
             <Link to="/game" style={{ color: location.pathname === '/game' ? '#9F34FF' : '#ffffff','--item': '0' }}>Game</Link>
+            <Link to="/search" style={{ color: location.pathname === '/search' ? '#9F34FF' : '#ffffff','--item': '0' }}>Search</Link>
           </div>
         </nav>
       </header>
