74320baa9a979485b9e1082c0d4b4a8a33be774d
Kiyodo <tyler.odo@gmail.com>
2024-03-14 22:23:06 -0700

    Home-Game-Card component: imported game preview image + added card container for testing

--- client/src/components/Home-Game-Card.jsx
+++ client/src/components/Home-Game-Card.jsx
@@ -1,53 +1,58 @@
+import gamePreview from 'client/src/assets/images/png/game-preview.png';
+
 const HomeGameCard = ({game}) => {
   return (
-    <article className="home-game-card">
-      <div>
-        <img src="" alt="game preview" />
-      </div>
+    // Testing a card container (div) to use flex and gap for each card
+    <div className="home-flex home-gap">
+      <article className="home-game-card">
+        <div>
+          <img src={gamePreview} alt="game preview" />
+        </div>
 
-      <div>
-        <h4>
-          {game.title}
-        </h4>
-      </div>
+        <div>
+          <h4>
+            {game.title}
+          </h4>
+        </div>
 
-      <div className="home-user-review-rating">
-        <div className="home-user-review">
-          <div>
-            <img src="" alt="profile pic" />
+        <div className="home-user-review-rating">
+          <div className="home-user-review">
+            <div>
+              <img src="" alt="profile pic" />
+            </div>
+            <div>
+              <h5>
+                {game.username}
+              </h5>
+              <p>
+                {game.review}
+              </p>
+            </div>
           </div>
-          <div>
-            <h5>
-              {game.username}
-            </h5>
-            <p>
-              {game.review}
-            </p>
+
+          <div className="home-user-rating">
+            <div>
+              <img src="" alt="star" />
+              <h5>
+                {game.rating}
+              </h5>
+            </div>
+            <div>
+              <img src="" alt="heart" />
+              <h5>
+                {game.likes}
+              </h5>
+            </div>
           </div>
         </div>
 
-        <div className="home-user-rating">
-          <div>
-            <img src="" alt="star" />
-            <h5>
-              {game.rating}
-            </h5>
-          </div>
-          <div>
-            <img src="" alt="heart" />
-            <h5>
-              {game.likes}
-            </h5>
-          </div>
+        <div>
+          <button className="home-game-button">
+            View Details
+          </button>
         </div>
-      </div>
-
-      <div>
-        <button className="home-game-button">
-          View Details
-        </button>
-      </div>
-    </article>
+      </article>
+    </div>
   )
 }
 
