4f7ec42872060a609322786c1296a81b5b04cdd5
Jorge Duarte <sadujorge@gmail.com>
2024-03-18 14:34:35 -0700

    Finished Sign up form styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // Components
 import App from './App.jsx'
+import Profile from './pages/Profile';
 import Home from './pages/Home';
 import Landing from './pages/Landing';
 import Game from './pages/Game';
@@ -13,6 +14,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { index: true, element: <Landing /> },
+      { path: 'Profile', element: <Profile /> },
       { path: 'Home', element: <Home /> },
       { path: 'Game', element: <Game /> },
       // { path: 'SearchPage', element: <SearchPage /> },
