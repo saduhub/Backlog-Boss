798f91e63d6b920caff524a6723b3c732dad35ca
Kaiwen <ArthurJStone96@gmail.com>
2024-03-18 03:20:29 -0500

    finished game review styling, added base for game card component

---
+++ client/src/components/GameCard.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+import gamePreview from '../assets/images/png/game-preview.png';
+
+const GameCard = ({game}) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default GameCard;
