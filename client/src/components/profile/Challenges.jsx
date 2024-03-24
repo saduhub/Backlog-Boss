a6468db8d5c83245c6fecadacec506ee5d63cac2
Jorge Duarte <sadujorge@gmail.com>
2024-03-24 12:26:08 -0700

    Challenges components now track likes

--- client/src/components/profile/Challenges.jsx
+++ client/src/components/profile/Challenges.jsx
@@ -1,10 +1,22 @@
+import { useQuery} from "@apollo/client";
+import { USER_LIKES } from "../../utils/queries";
 // eslint-disable-next-line
 function Challenges({logo, otherData}) {
+    const userId = localStorage.getItem('_id');
+    const { data, loading, error } = useQuery(USER_LIKES, {
+        variables: { userReviewsId: userId },
+    });
+    console.log(data)
+    const likesData = data?.userReviews || []
+    console.log(error);
+    console.log(likesData);
     // eslint-disable-next-line
     const reviewedGamesCount = otherData.reviews.length;
-    const likesCount = 100;
+    const likesCount = likesData.reduce((total, current) => total + current.likes, 0);
     const reviewedGamesWidth = `${(reviewedGamesCount / 50) * 100}%`;
     const likesWidth = `${(likesCount / 200) * 100}%`;
+
+    if (loading) return <p>Loading...</p>;
 
     return (
         <div className="profile-challenges">
