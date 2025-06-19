a8d550620e4f827a1b7c141ed4b5ec73cd70108b
Jorge Duarte <sadujorge@gmail.com>
2025-06-18 19:28:15 -0700

    Added wrapper function to all mutations. Tested and confrimed they all work with the exception of addLikeToReview. This is a previous issue as the wrapper does not cuase the bug. Will fix in future commit.

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ const server = new ApolloServer({
     resolvers,
     // In production, I return a generic error message and overwrite all error codes with "SERVER_ERROR".This is a deliberate security choice to avoid leaking internal logic, database structure, or API behavior. In development, I expose full error details to aid debugging.
     // TODO: In the future, implement logging of full internal errors (e.g., using Sentry, LogRocket, or a file-based logger) to ensure visibility when detailed error messages are hidden from the client. 
-    //For the time being, a resolver error witl return a data object inside of the error response. No data object means that it is an error at the the apollo server level, such as a syntax error in the query or a problem with the server configuration..
+    //For the time being, a resolver error witl return a data object inside of the error response. No data object means that it is an error at the the apollo server level, such as a syntax error in the query or a problem with the server configuration.
+    // I currently perform auth checks manually in each resolver, but since wrapResolver masks all errors, failed auth returns a generic message for now. I plan to abstract auth globally in the future.
     formatError: (err) => {
         const isDev = process.env.NODE_ENV !== 'production';
         const baseError = {
